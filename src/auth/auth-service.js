97889f2f409df99e57ddc6426e30b9036bcc1196
Angus McNair <amcnair234@gmail.com>
2020-01-24 14:54:01 -0600

    Final commit for adding JWT authentication

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -19,7 +19,7 @@ const AuthService = {
     },
     verifyJwt(token){
         return jwt.verify(token, config.JWT_SECRET, {
-            algorithms: ['HS256']
+            algorithms: ['HS256'],
         })
     },
     parseBasicToken(token){
